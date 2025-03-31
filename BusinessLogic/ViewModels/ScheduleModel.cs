696be5f3be9966d9d82e169406b13e8ca1aa5e50
Dev-FStacker <bennguyen.contact@gmail.com>
2025-03-31 20:51:39 +0700

    feat: change schedule

---
+++ BusinessLogic/ViewModels/ScheduleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.ViewModels
+{
+    public class ScheduleModel
+    {
+        public int ScheduleId { get; set; }
+        public int SlotId { get; set; }
+        public string SlotTime { get; set; }
+        public DateOnly ScheduleDate { get; set; }
+    }
+}
